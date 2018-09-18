154090fb887aca4f038dd5814a2392366cda9601
DeuterGraves <puckles@gmail.com>
2018-09-18 09:58:52 +0100

    notes from review added

--- team/specs/team_specs.rb
+++ team/specs/team_specs.rb
@@ -32,15 +32,25 @@ end
 
 def test_add_player_to_array
   team = HockeyTeam.new("Caps", "Todd Reirden", 0)
+  #new_player = "Ovechkin" <- you can avoid reusing the string by just storing it in a variable in the test. 
   team.add_player("Ovechkin")
   team.search_players("Ovechkin")
   assert_equal(true, team.search_players("Ovechkin"))
+  # assert_equal(true, team.players().include?("Ovechkin"))
+  # if asserting true you don't need to include _equal
+  # assert(team.players().include?(new_player))
 end
 
 def test_team_points
   team = HockeyTeam.new("Caps", "Todd Reirden", 0)
   team.win_points(2)
   assert_equal(2, team.points)
+end
+
+def test_add_multiple_player()
+  @team.add_many(["Craig Morton", "Nyalls Hemingway"])
+  expected = ["Craig Morton", "Nyalls Hemingway"]
+  assert_equal(expect, @team.players)
 end
 
 
--- team/team_class.rb
+++ team/team_class.rb
@@ -4,6 +4,7 @@ class HockeyTeam
   attr_reader
 
   def initialize(team, coach, points)
+    #@team is called the instance variable
     @team = team
     @players = []
     @coach = coach
@@ -15,14 +16,27 @@ class HockeyTeam
     @players.push(player_name)
   end
 
+# adding an array to an array
+  def add_many(new_players)
+    #this function is being passed an array so loop!
+    for player in new_players
+      # @players << (player)
+      add_player(player)
+      ##OR  - issue with this method is if you change add players to ban someone for example it won't apply here
+      #@players.concat(new_players)
+end
+
+# good idea for this type of method/fuciton - put a ? in the name i.e. has_players?
   def search_players(player_name)
-    for player in @players
-      if player == player_name
-        return true
-      else
-        return false
-      end
-    end
+    # for player in @players
+      # if player == player_name
+      #   return true
+      # else
+      #   return false
+      # end
+    # end
+    # better way to write - had implicit true/false result
+    return @players.include?(player)
   end
 
 def win_points(num)
