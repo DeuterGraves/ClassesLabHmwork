60bf8184b4ccca467e0c1cd3f7b58d00213f21af
DeuterGraves <puckles@gmail.com>
2018-09-22 22:41:12 +0100

    did library exercise for hashes evidence for PDA

--- library/library.rb
+++ library/library.rb
@@ -1,20 +1,40 @@
-class library
+class Library
 
-def initialize(book_array)
-  @book_list = book_array
+  attr_accessor :book_list
+
+  def initialize(book_list)
+    @book_list = book_list
+  end
+
+  # def book_details()
+  #   @book_list[0]
+  # end
+
+  def search_by_title(title)
+    for book in @book_list
+      if book[:title] == title
+        return book
+      end
+    end
+  end
+
+def search_rental_details_by_title(title)
+  for book in @book_list
+    if book[:title] == title
+      return book[:rental_details]
+    end
+  end
 end
 
+def add_book(new_book)
+  @book_list << new_book
+end
 
-
-
-
-
-
-
-
-
-
-
+def update_rental_info(book, renter, due_date)
+  rented_book = search_by_title(book)
+  rented_book[:rental_details][:student_name] = renter
+  rented_book[:rental_details][:date] = due_date
+end
 
 
 end
--- library/specs/library_spec.rb
+++ library/specs/library_spec.rb
@@ -1,15 +1,17 @@
 require('minitest/autorun')
 require('minitest/rg')
+require("pry")
 require_relative('../library')
 
 class TestLibrary < MiniTest::Test
 
   def setup
+
     #create array here
-    [
+    @book_list = [
       {
         title: "I'll Never Get Out of this World Alive",
-        author: "Steve Earle"
+        author: "Steve Earle",
         rental_details: {
           student_name: "Maisie",
           date: "28/12/17"
@@ -17,7 +19,7 @@ class TestLibrary < MiniTest::Test
       },
       {
         title: "Born a Crime",
-        author: "Trevor Noah"
+        author: "Trevor Noah",
         rental_details: {
           student_name: "Cynthia",
           date: "17/08/18"
@@ -25,34 +27,92 @@ class TestLibrary < MiniTest::Test
       },
       {
         title: "Car Sick: John Waters Hitchhikes Across America",
-        author: "John Waters"
+        author: "John Waters",
         rental_details: {
           student_name: "Jamie",
           date: "10/09/2018"
         }
+      },
+      {
+        title: "Unfamiliar Fishes",
+        author: "Sarah Vowel",
+        rental_details:{
+          student_name: " " ,
+          date: ""
+        }
+      },
+      {
+        title: "Down by the River Where the Dead Men Go",
+        author: "George Pelecanos",
+        rental_details:{
+          student_name: " " ,
+          date: ""
+        }
+      },
+      {
+        title: "The Girl with All the Gifts",
+        author: "M.R. Carey",
+        rental_details:{
+          student_name: " " ,
+          date: ""
+        }
+      },
+      {
+        title: "When You Are Engulfed in Flames",
+        author: "David Sedaris",
+        rental_details:{
+          student_name: " ",
+          date: " "
+        }
       }
     ]
+
+    @library = Library.new(@book_list)
+
     # setup end
   end
 
-# get the books - full details
+  # getter for books
+  def test_get_book_details()
+    assert_equal("Steve Earle", @book_list[0][:author])
+  end
+
+  # get the books - full details
+  def test_search_book_by_title()
+    result = @library.search_by_title("Born a Crime")
+    assert_equal(@book_list[1], result)
+  end
+  #
+  # # get book 2's rental details by title
+  def test_search_book_rental_by_title()
+    result = @library.search_rental_details_by_title("Born a Crime")
+    assert_equal(@book_list[1][:rental_details], result)
+  end
 
 
-#search book by name return book details
+  # add new book hash (renter and due date keys with no values yet)
+def test_add_new_book()
+  new_book = {
+    title: "True Grit",
+    author: "Charles Portis",
+    rental_details:{
+      student_name: " ",
+      date: " "
+    }
+  }
+    @library.add_book(new_book)
+    assert_equal(new_book, @book_list.last)
+  end
 
-# search book by name return rental details
+  # update the rental information for a book
 
-# add new book hash (renter and due date keys with no values yet)
-
-# update the rental information for a book
-
-
-
-
+  def test_update_rental_info()
+    @library.update_rental_info("Unfamiliar Fishes", "Katie", "12/02/2019")
+    assert_equal("Katie", @book_list[3][:rental_details][:student_name])
+    assert_equal("12/02/2019", @book_list[3][:rental_details][:date])
+  end
 
 
 
-
-
-#class end
+  #class end
 end
